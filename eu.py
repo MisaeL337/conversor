da847302ec8494acceb1c0ee01e8b6c685537ccc
MisaeL337 <152302609+MisaeL337@users.noreply.github.com>
2024-01-04 14:03:35 -0300

    conversor_de_moeda.py

--- eu.py
+++ eu.py
@@ -1,20 +1,34 @@
+print('-'*50)
+print('BEM VINDO A NOSSA CALCULADORA DE CONVERÇÃO DE MOEDA')
+print('-'*50)
+
+
+print('PARA COMEÇAR, RESPONDA AS SEGUINTES PERGUNTAS: ')
 while True:
     try:
-        dinheiro = input('você quer converter o seu dinheiro em "dolar" ou  em "euro"?')
-        valor = float(input('ok, agora digite o valor em reais que deseja converter: '))   
+        nome = input("Por favor, digite o seu nome para sabermos melhor sobre você: ").upper()
+        dinheiro = input(f'Ola {nome}, como deseja converter o seu dinherio? em "dolar" ou  em "euro"?: ').lower()
+        valor = float(input(f'ok {nome}, agora digite o valor em reais que deseja converter: ')) 
         calcule = 0
         calcule1 = 0
         if dinheiro == 'dolar':
             calcule = valor / 4.91
-            print(f'pronto, {valor} reias é o equivalante a {calcule:.2f} dolares')
+            print(f'pronto {nome}, {valor:.0f} reias é o equivalante a {calcule:.2f} dolares')
         elif dinheiro == 'euro':
             calcule1 = valor / 5.37
-            print(f'pronto, {valor} reais é o equivalante a {calcule1:.2f} euros')         
+            print(f'pronto {nome}, {valor:.0f} reais é o equivalante a {calcule1:.2f} euros')   
+        else:
+            print(f'por favor {nome}, digite somente o nome da moeda que deseja converter, "dolar" ou "euro" ')        
 
-        final = input(f'se deseja continuar digite "sim" caso contrario, digite "nao"')
+
+        final = input(f'se deseja continuar {nome}, digite "sim" caso contrario, digite "nao" ').lower()
         if final == 'sim':
             continue        
         elif final == 'nao':
-            break        
+            break    
+    except ValueError:
+        print(f'por favor {nome}, digite somente números inteiros')
     finally:
-        print('muito obrigado por usar nosso progama')
+        print(f'muito obrigado por usar nosso progama {nome}, até a proxima')
+
+
